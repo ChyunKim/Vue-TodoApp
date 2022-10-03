b28ab8fa75f6bbe37f9b5c2766f79d0af0943bc7
ChyunKim <co1995@naver.com>
2022-10-03 17:05:58 +0900

    INITIAL COMMIT

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Todo msg="TODO LIST" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todo
   }
 }
 </script>
